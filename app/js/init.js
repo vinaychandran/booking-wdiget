f648d41b7ff8eb0d573c68d1ca95501c14d9fcf1
Pavan Balakrishna <pavan.hbalakrishna@gmail.com>
2018-08-28 12:29:18 +0900

    Moved logic to set dates it its own function

--- app/js/init.js
+++ app/js/init.js
@@ -1,4 +1,30 @@
 var MystaysBookingWidget = {
+
+    SetDateValues: function SetDateValues(startval, endval) {
+        if (startval !== "" && startval) {
+            var startMonth = startval.split('|')[0];
+            var startDate = startval.split('|')[1];
+            var startDay = startval.split('|')[2];
+
+            var checkinTitle = document.querySelectorAll('#bookingwidget-checkin .title');
+            checkinTitle[0].innerHTML = startMonth;
+
+            var checkinDesc = document.querySelectorAll('#bookingwidget-checkin .desc');
+            checkinDesc[0].innerHTML = startDate + " " + startDay;
+        }
+
+        if (endval !== "" && endval) {
+            var endMonth = endval.split('|')[0];
+            var endDate = endval.split('|')[1];
+            var endDay = endval.split('|')[2];
+
+            var checkoutTitle = document.querySelectorAll('#bookingwidget-checkout .title');
+            checkoutTitle[0].innerHTML = endMonth;
+
+            var checkoutDesc = document.querySelectorAll('#bookingwidget-checkout .desc');
+            checkoutDesc[0].innerHTML = endDate + " " + endDay;
+        }
+    },
     IsMobile: function IsMobile() {
         return window.innerWidth < 767;
     },
@@ -44,29 +70,7 @@ var MystaysBookingWidget = {
                 var startval = inst.startVal;
                 var endval = inst.endVal;
 
-                if (startval !== "" && startval) {
-                    var startMonth = startval.split('|')[0];
-                    var startDate = startval.split('|')[1];
-                    var startDay = startval.split('|')[2];
-
-                    var checkinTitle = document.querySelectorAll('#bookingwidget-checkin .title');
-                    checkinTitle[0].innerHTML = startMonth;
-
-                    var checkinDesc = document.querySelectorAll('#bookingwidget-checkin .desc');
-                    checkinDesc[0].innerHTML = startDate + " " + startDay;
-                }
-                
-                if (endval !== "" && endval) {
-                    var endMonth = endval.split('|')[0];
-                    var endDate = endval.split('|')[1];
-                    var endDay = endval.split('|')[2];
-
-                    var checkoutTitle = document.querySelectorAll('#bookingwidget-checkout .title');
-                    checkoutTitle[0].innerHTML = endMonth;
-
-                    var checkoutDesc = document.querySelectorAll('#bookingwidget-checkout .desc');
-                    checkoutDesc[0].innerHTML = endDate + " " + endDay;
-                }
+                MystaysBookingWidget.SetDateValues(startval, endval);
 
                
             },
